0c7c6d59ec1525f860510ddfa634aef747ebc20e
cjmunozy <cjmunoz@espol.edu.ec>
2023-07-12 00:47:15 -0500

    Creación del repositorio para el proyecto

---
+++ src/main/java/ec/edu/espol/proyectoparcial/ProyectoParcial.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espol.proyectoparcial;
+
+/**
+ *
+ * @author crisj
+ */
+public class ProyectoParcial {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
