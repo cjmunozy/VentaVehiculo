39652a685b745bceeca09a98a857130c6f4a1fac
cjmunozy <cjmunoz@espol.edu.ec>
2023-07-12 01:05:03 -0500

    Ruta del proyecto renombrada, paquetes y clase Vehiculo creados.

--- src/main/java/ec/edu/espol/ventavehiculo/VentaVehiculo.java
+++ src/main/java/ec/edu/espol/ventavehiculo/VentaVehiculo.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package ec.edu.espol.proyectoparcial;
+package ec.edu.espol.ventavehiculo;
 
 /**
  *
  * @author crisj
  */
-public class ProyectoParcial {
+public class VentaVehiculo {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
